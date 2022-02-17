147cb4d965864081707621ece2fd765a19d01f6b
banjman81 <benjamin.man97.com>
2022-02-17 11:24:31 -0600

    home page populated

--- src/App.js
+++ src/App.js
@@ -12,26 +12,36 @@ import { ProductsContext, CartContext } from './components/contexts/ProductsCont
 
 function App() {
   const [products, setProducts] = useState()
-  const [cart, setCart] = useState()
+  const [cart, setCart] = useState([])
 
   useEffect(() => {
     const fetchProducts = async () => {
       const payload = await axios.get('https://fakestoreapi.com/products')
-      console.log(payload.data)
+      setProducts(payload.data)
     }
 
     fetchProducts()
   }, [])
+
+  const addToCart = (id) => {
+    const addedItem = products.find(product => product.id == id)
+    console.log(addedItem.id)
+    setCart([...cart, addedItem])
+    console.log(cart)
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <ProductsContext.Provider value={{products, addToCart}}>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/cart" element={<Cart/>}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/signup" element={<SignUp/>}></Route>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+            <Route path="/signin" element={<SignIn />}></Route>
+            <Route path="/signup" element={<SignUp/>}></Route>
         </Routes>
+        </ProductsContext.Provider>
       </BrowserRouter>
     </div>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Button } from '@mui/material';
+import { ProductsContext } from '../contexts/ProductsContext'
 
 function Home() {
+
+    const {products, addToCart} = useContext(ProductsContext)
     return (
         <div>
-            <h1>Home Page</h1>
+            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start'}}>
+                {products ? products.map(product => {
+                    return (
+                        <div style={{border: '1px solid black', margin: '5px auto', padding: '5px', borderRadius : '3px', width : '20%', height: '300px'}} key={product.id}>
+                            <div style={{display:'flex',flexDirection: 'column', justifyContent: 'space-evenly'}}>
+                                <div style={{display:'flex', flexDirection: 'row-reverse'}}> 
+                                    <FavoriteIcon />
+                                </div>
+                                <div style={{height: '100px'}}>
+                                    <img style={{paddingTop: '25px', maxWidth:"95%"}} src={product.image} alt="" height="70%"/>
+                                </div>
+                                <div>
+                                    <h5 style={{height: '50px'}}>{product.title}</h5>
+                                    <p style={{color: 'green'}}>${product.price}</p>
+                                </div>
+                                <Button variant="contained" onClick={() => addToCart(product.id)}>Add to cart</Button>
+                            </div>
+                            
+                        </div>
+                    )
+                }): "Loading..."}
+            </div>
         </div>
     )
 }
