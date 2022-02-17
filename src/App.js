f7b9b2a3023520adb967ec7798430fa316c5f290
banjman81 <benjamin.man97.com>
2022-02-17 09:38:24 -0600

    axios get api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
@@ -7,9 +8,20 @@ import Cart from './components/pages/Cart';
 import SignUp from './components/pages/SignUp';
 import SignIn from './components/pages/SignIn';
 
+import { ProductsContext, CartContext } from './components/contexts/ProductsContext';
+
 function App() {
   const [products, setProducts] = useState()
   const [cart, setCart] = useState()
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const payload = await axios.get('https://fakestoreapi.com/products')
+      console.log(payload.data)
+    }
+
+    fetchProducts()
+  }, [])
   return (
     <div className="App">
       <BrowserRouter>
